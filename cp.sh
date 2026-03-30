1698d535323afbfc47048dc45d834e23acceb251
sunguangrun <sunguangrun22@mails.ucas.ac.cn>
2026-03-30 19:35:30 +0000

    Improve the compilation process

---
+++ cp.sh
@@ -0,0 +1,3 @@
+scons setconfig build/NULL RUBY_PROTOCOL_Garnet_standalone=y
+scons setconfig build/NULL RUBY_PROTOCOL_MI_example=n
+scons setconfig build/NULL PROTOCOL=Garnet_standalone
--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
+# ./build/NULL/gem5.opt configs/example/garnet_synth_traffic.py --num-cpus=4 --num-dirs=4 --network=garnet --topology=Mesh_XY --mesh-rows=2 --sim-cycles=10000000 --synthetic=uniform_random --injectionrate=0.04
+
+
 /usr/bin/python3 util/sweep_garnet_injection.py \
     --gem5-bin ./build/NULL/gem5.opt \
     --config configs/example/garnet_synth_traffic.py \
     --extractor util/extract_garnet_stats.py \
-    --num-cpus 4 \
-    --num-dirs 4 \
+    --num-cpus 16 \
+    --num-dirs 16 \
     --network garnet \
     --topology Mesh_XY \
-    --mesh-rows 2 \
+    --mesh-rows 4 \
     --sim-cycles 10000000 \
     --synthetic uniform_random \
     --min-rate 0 \
     --max-rate 1 \
     --coarse-step 0.01 \
     --max-fine-intervals 0 \
-    --workdir m5out/sweep_step_0p01_uniform \
+    --workdir m5out/sweep_RR_0p01_uniform \
     --force-rerun
